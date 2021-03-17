8fa508fce63ea373db9f143b2f6d3e095b4c4e97
Code4Sheep <62000405+Code4Sheep@users.noreply.github.com>
2021-03-16 19:24:43 -0500

    Delete lab6 directory

--- lab6/watermark_image.m
+++
@@ -1,8 +0,0 @@
-function watermark_image(uin)
-% watermark_image(UIN) print UIN in bottom left corner of image of current
-% axes
-%   UIN is a number.  There is no output.  
-	ax = gca; 
-	t = text(min(ax.XLim),max(ax.YLim)-4,num2str(uin)); 
-	t.Color = [0.7 0.7 0.7]; t.FontSize = 14; 
-end
